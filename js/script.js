c51bc3525db05510b521516054d751b85a5701ea
Monika Gill <meenugill2603@gmail.com>
2025-08-17 16:31:16 +0530

    initial changes in website

--- js/script.js
+++ js/script.js
@@ -32,3 +32,23 @@ let images = document.querySelectorAll(".slider img");
             }, 2000); // Adjust time as needed
         };
 
+        // Navbar scroll behavior for desktop
+        window.addEventListener('scroll', function() {
+            const navbar = document.querySelector('.navbar');
+            const header = document.querySelector('.desktop-header');
+            
+            if (window.innerWidth > 768) { // Only for desktop
+                if (header) {
+                    const headerHeight = header.offsetHeight;
+                    
+                    if (window.scrollY > headerHeight) {
+                        // When scrolled past header, move navbar to top
+                        navbar.style.top = '0px';
+                    } else {
+                        // When at top, position navbar below header
+                        navbar.style.top = headerHeight + 'px';
+                    }
+                }
+            }
+        });
+
